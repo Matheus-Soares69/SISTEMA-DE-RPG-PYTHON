a396e22236bafaa049de7e653ddcb7e171bae64b
Matheus-Soares69 <matheussa862@gmail.com>
2026-06-09 12:27:03 -0300

    Pimeira versão do RPG

---
+++ SISTEMA_DE_RPG.py
@@ -0,0 +1,87 @@
+
+class Personagem:
+    def __init__(self,nome, classe, vida, dano):
+        self.nome = nome
+        self.classe = classe
+        self.vida = vida
+        self.dano = dano
+    def atacar(self, alvo):
+        alvo.vida -= self.dano
+        print(f"{self.nome} atacou {alvo.nome}!")
+        print(f"{alvo.nome} perdeu {self.dano} de vida!")
+        print(f"Vida restante de {alvo.nome}: {alvo.vida}")
+        if alvo.vida <= 0:
+            print(f"{alvo.nome} desmaiou!")
+        
+        
+class Mago(Personagem):
+    def __init__(self, nome, classe, vida, dano, mana):
+        super().__init__(nome, "Mago", vida, dano)
+        self.mana = mana
+        
+    def lancar_magia(self, alvo):
+        if self.mana >= 50:
+            print("Bola de fogo lançada!")
+            self.mana -= 50
+            print(f"Mana restante {self.mana}")
+        else:
+            print("Mana insuficiente!")    
+  
+  
+class Heroi(Personagem):
+    def __init__(self, nome, classe, vida, dano, força):
+        super().__init__(nome, "Heroi", vida, dano)
+        self.força = força
+        
+    def ataque_circular(self, alvo):
+        if self.força >= 75:
+            print("HYAAAAAA")
+            self.força -= 75
+            print(f"Forca restante {self.força}")
+        else:
+            print("Força insuficiente")
+        
+        
+class Paladino(Personagem):
+    def __init__(self, nome, classe, vida, dano, mana, fé):
+        super().__init__(nome, "Paladino", vida, dano)
+        self.mana = mana
+        self.fé = fé
+    
+    def rezar(self, alvo):
+        self.alvo = alvo
+        if self.fé >= 150:
+            print("Que Deus tenha misericórdia de tua alma!!")
+            self.fé -= 150
+            print(f"Fé restante {self.fé}")
+        else:
+            print("Fé insuficiente")
+    
+class Nekomata(Personagem):
+    def __init__(self, nome, classe, vida, dano, mana, arte_marcial):
+        super().__init__(nome, "Nekomata", vida, dano)
+        self.arte_marcial = arte_marcial 
+        self.mana = mana
+        
+  
+    def arranhada(self, alvo):
+        self.alvo = alvo
+        if self.mana >= 65:
+            print("Arranhada na cara hehe!")
+            self.mana -= 65
+            print(f"Mana restante {self.mana}")
+        else:
+            print("Mana insuficante!")    
+  
+  
+dumbledore = Mago("dumbledore", "mago", 50, 150, 300)  
+link = Heroi("Link", "Heroi", 250, 50, 500)
+uriel = Paladino("Uriel", "Paladino", 240, 180, 200, 500)
+tigresa = Nekomata("Tigresa", "Nekomata", 220, 50, 300, "Kung-Fu")
+
+
+
+link.atacar(dumbledore)
+dumbledore.atacar(tigresa)
+tigresa.atacar(uriel)
+uriel.atacar(link)
